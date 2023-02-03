8d38f48d85cc546ca33ca2dd7fe186765f85e8f5
OsazeeOsaro <67610416+OsazeeOsaro@users.noreply.github.com>
2023-02-03 12:56:36 -0500

    idk
    idk

--- src/main/java/ca/warp7/frc2023/RobotContainer.java
+++ src/main/java/ca/warp7/frc2023/RobotContainer.java
@@ -4,15 +4,31 @@
 
 package ca.warp7.frc2023;
 
+import ca.warp7.frc2023.Commands.spining;
+import ca.warp7.frc2023.SubSystems.intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
+
+    intake intakeing = new intake();
+    spining spin = new spining(intakeing);
+    CommandXboxController exampleCommandController = new CommandXboxController(1); 
+    Trigger xButton = exampleCommandController.x(); 
+
     public RobotContainer() {
         configureBindings();
     }
 
-    private void configureBindings() {}
+    
+    private void configureBindings() {
+     exampleCommandController.x().toggleOnTrue(intakeing.movemotorCommand());
+     exampleCommandController.a().toggleOnTrue(intakeing.stopmotorCommand());
+     exampleCommandController.b().toggleOnTrue(intakeing.reversemotorCommand());
+       
+    }
 
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
