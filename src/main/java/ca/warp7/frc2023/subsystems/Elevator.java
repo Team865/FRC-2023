dee2b31f5ba0490b9d7c07b7f83afd0a38df94bb
png <gamingwithcole7@gmail.com>
2023-02-03 18:01:49 -0500

    Create 4 SparkMax motor controllers
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>

--- src/main/java/ca/warp7/frc2023/subsystems/Elevator.java
+++ src/main/java/ca/warp7/frc2023/subsystems/Elevator.java
@@ -1,6 +1,10 @@
 package ca.warp7.frc2023.subsystems;
 
+import static ca.warp7.frc2023.Constants.*;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 
 public class Elevator extends SubsystemBase {
     private static Elevator instance;
@@ -11,4 +15,38 @@ public class Elevator extends SubsystemBase {
         return instance;
     }
 
+    public static CANSparkMax createMasterSparkMAX(int deviceID) {
+        CANSparkMax master = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushed);
+        master.restoreFactoryDefaults();
+        master.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        master.enableVoltageCompensation(12.0);
+        return master;
+    }
+
+    public CANSparkMax createFollowerSparkMAX(int deviceID) {
+        CANSparkMax follower = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushed);
+        follower.restoreFactoryDefaults();
+        follower.follow(LeftMain);
+        follower.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        follower.enableVoltageCompensation(12.0);
+        return follower;
+    }
+
+    // creates an inverted SparkMAx follower motor
+    public CANSparkMax createFollowerSparkMAXInverted(int deviceID) {
+        CANSparkMax follower = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushed);
+        follower.restoreFactoryDefaults()  ;
+        follower.follow(LeftMain, true);
+        follower.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        follower.enableVoltageCompensation(12.0);
+        return follower;
+    }
+
+    // makes a leader and a follower motor for the left side
+    public CANSparkMax LeftMain = createMasterSparkMAX(kElevatorLeftMain);
+    public CANSparkMax LeftSecondary = createFollowerSparkMAX(kElevatorLeftSecondary);
+
+    // makes the inverted follower motors for the right side
+    public CANSparkMax RightMain = createFollowerSparkMAXInverted(kElevatorRightMain);
+    public CANSparkMax RightSecondary = createFollowerSparkMAXInverted(kElevatorRightSecondary);
 }
