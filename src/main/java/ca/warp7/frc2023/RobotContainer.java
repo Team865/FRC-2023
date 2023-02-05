b487a6667eb133ab5d362200e9200ab0aa057e33
OsazeeOsaro <67610416+OsazeeOsaro@users.noreply.github.com>
2023-02-05 08:23:33 -0500

    edir

--- src/main/java/ca/warp7/frc2023/RobotContainer.java
+++ src/main/java/ca/warp7/frc2023/RobotContainer.java
@@ -6,28 +6,39 @@ package ca.warp7.frc2023;
 
 import ca.warp7.frc2023.Commands.spining;
 import ca.warp7.frc2023.SubSystems.intake;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
 
     intake intakeing = new intake();
     spining spin = new spining(intakeing);
-    CommandXboxController exampleCommandController = new CommandXboxController(1); 
-    Trigger xButton = exampleCommandController.x(); 
+    int toggle = 0;
+    CommandXboxController controller = new CommandXboxController(1); 
+    XboxController jController = new XboxController(2);
+ 
+    
+    
 
     public RobotContainer() {
+        if (jController.getYButtonPressed() ) {
+            System.out.println();
+            intakeing.movemotorCommand();
+        }
         configureBindings();
     }
 
     
     private void configureBindings() {
-     exampleCommandController.x().toggleOnTrue(intakeing.movemotorCommand());
-     exampleCommandController.a().toggleOnTrue(intakeing.stopmotorCommand());
-     exampleCommandController.b().toggleOnTrue(intakeing.reversemotorCommand());
-       
+        controller.b().toggleOnTrue(intakeing.toggle());
+        controller.x().toggleOnTrue(intakeing.movemotorCommand());
+        controller.a().toggleOnTrue(intakeing.stopmotorCommand());
+        
+    
+
     }
 
     public Command getAutonomousCommand() {
