6d3f1fae788ca0d5bcf1c9df39c022c69e28bb9c
KateBin™ <72105928+Katebin@users.noreply.github.com>
2023-02-07 14:35:08 -0800

    Update

--- src/main/java/ca/warp7/frc2023/subsystems/Fourbar.java
+++ src/main/java/ca/warp7/frc2023/subsystems/Fourbar.java
@@ -1,22 +1,33 @@
 package ca.warp7.frc2023.subsystems;
 import static ca.warp7.frc2023.Constants.*;
-import edu.wpi.first.math.controller.PIDController;
+import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 public class Fourbar extends SubsystemBase {
     /* fourbar subsytem, used as base to move with PID and remain in requested location
-    TODO:
+    TO DO:
         - setup PID
         - get PID to automove
         - account for gravity */
-    private final TalonFX[] motors = { new TalonFX(FOURBAR_MOTOR_PORT_0), new TalonFX(FOURBAR_MOTOR_PORT_1), new TalonFX(FOURBAR_MOTOR_PORT_2), new TalonFX(FOURBAR_MOTOR_PORT_3) };
-    private final PIDController pid = new PIDController(0, 0, 0);
+    private final TalonFX[] motors = { new TalonFX(FOURBAR_MOTOR_PORT_0), new TalonFX(FOURBAR_MOTOR_PORT_1) };
+    private static Fourbar instance;
     
-    public Fourbar() {}
+    public static Fourbar getInstance() {
+        if (instance == null) instance = new Fourbar();
+        return instance;
+    }
+
+    private void setMotors(double percent) {
+        // motors are connected, both should spin when needed
+        motors[0].set(ControlMode.PercentOutput, percent);
+        motors[1].set(ControlMode.PercentOutput, percent);
+    }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+    }
 
     @Override  
     public void simulationPeriodic() {}
