93d7c06b7d3c409d80305a8ef02ef3be285af31f
Cole Kauder-McMurrich <85180852+Colepng@users.noreply.github.com>
2023-02-20 16:15:40 -0500

    Create elevator subsystem (#7)
    * Updated REVlib
    
    * Made 4 SparkMaxs 
    
    Made 4 SparkMaxs for practice all master tho we dont talk about that
    
    * Almost made the entire subsystem
    
    made motor, pidcontroller limit switch and encoder but enocoder wrong type
    
    Co-Authored-By: SAC-4 <119978243+SAC-4@users.noreply.github.com>
    
    * Add 4 Constants for placeholder can ids 
    
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create outline of the elevator subsystem
    
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create 4 SparkMax motor controllers 
    
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create a limit switch
    
    Created a limit switch that connects to the LeftMain motor
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create an incremental encoder
    
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create a PID controller
    
    Created a PID controller that uses smart motion
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    Co-authored-by: Skii1 <Skii1@users.noreply.github.com>
    
    * Create a method
    
    Created a method that returns the number of rotations of the hex shaft.
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create a method
    
    Created a method that sets the setPoint of the PID controller to whatever was passed as the input.
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create a method
    
    Created a method that resets the encoder only if the limit switch is being pressed.
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Create a method 
    
    Created a method that fully retracts the elevator and then zeros the encoder.
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Fix formatting
    
    * Add info on SmartDashboard
    
    Co-authored-by: Skii1 <Skii1@users.noreply.github.com>
    
    * Change all objects to camel case
    
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    
    * Fix logic with fullyRetract
    
    * Fix motor names
    
    Co-authored-by: SAC-4 <SAC4@users.noreply.github.com>
    
    * Update kI and kD
    
    * Fix formatting
    
    * Fix logic
    
    Used wrong method
    
    * Fix motor names
    
    * Change most public to private
    
    * Formatting
    
    ---------
    
    Co-authored-by: SAC-4 <119978243+SAC-4@users.noreply.github.com>
    Co-authored-by: SAC-4 <SAC-4@users.noreply.github.com>
    Co-authored-by: Skii1 <Skii1@users.noreply.github.com>
    Co-authored-by: SAC-4 <SAC4@users.noreply.github.com>

--- src/main/java/ca/warp7/frc2023/Constants.java
+++ src/main/java/ca/warp7/frc2023/Constants.java
@@ -1,3 +1,10 @@
 package ca.warp7.frc2023;
 
-public class Constants {}
+public class Constants {
+    // Replace with real can id's
+    public static final int kMotorFirst = 11;
+    public static final int kMotorSecond = 12;
+
+    public static final int kMotorThird = 21;
+    public static final int kMotorFourth = 22;
+}
---
+++ src/main/java/ca/warp7/frc2023/subsystems/Elevator.java
@@ -0,0 +1,147 @@
+package ca.warp7.frc2023.subsystems;
+
+import static ca.warp7.frc2023.Constants.*;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.SparkMaxLimitSwitch;
+import com.revrobotics.SparkMaxLimitSwitch.Type;
+import com.revrobotics.SparkMaxPIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+    private static Elevator instance;
+
+    // returns the instance of the subsystem
+    public static Elevator getInstance() {
+        if (instance == null) instance = new Elevator();
+        return instance;
+    }
+
+    private static CANSparkMax createMasterSparkMAX(int deviceID) {
+        CANSparkMax master = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushed);
+        master.restoreFactoryDefaults();
+        master.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        master.enableVoltageCompensation(12.0);
+        return master;
+    }
+
+    private CANSparkMax createFollowerSparkMAX(int deviceID) {
+        CANSparkMax follower = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushed);
+        follower.restoreFactoryDefaults();
+        follower.follow(motorFirst);
+        follower.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        follower.enableVoltageCompensation(12.0);
+        return follower;
+    }
+
+    // creates an inverted SparkMAx follower motor
+    private CANSparkMax createFollowerSparkMAXInverted(int deviceID) {
+        CANSparkMax follower = new CANSparkMax(deviceID, CANSparkMaxLowLevel.MotorType.kBrushed);
+        follower.restoreFactoryDefaults();
+        follower.follow(motorFirst, true);
+        follower.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        follower.enableVoltageCompensation(12.0);
+        return follower;
+    }
+
+    // makes a leader and a follower motor for the left side
+    private CANSparkMax motorFirst = createMasterSparkMAX(kMotorFirst);
+    private CANSparkMax motorSecond = createFollowerSparkMAX(kMotorSecond);
+
+    // makes the inverted follower motors for the right side
+    private CANSparkMax motorThird = createFollowerSparkMAXInverted(kMotorThird);
+    private CANSparkMax motorFourth = createFollowerSparkMAXInverted(kMotorFourth);
+
+    // Check if limit switch is forward or Reverse irl
+    // Check if we are going to use Normally closed or Normally open
+    // used to sync with encoders
+    private SparkMaxLimitSwitch limitSwitchMotorFirst = motorFirst.getForwardLimitSwitch(Type.kNormallyClosed);
+
+    // Creates an incremental encoder
+    private RelativeEncoder encoder = motorFirst.getAlternateEncoder(SparkMaxAlternateEncoder.Type.kQuadrature, 8192);
+    // Method that creates and sets up a PID controller
+    private SparkMaxPIDController createAndSetupPIDController(CANSparkMax motor) {
+        SparkMaxPIDController PIDController = motor.getPIDController();
+        PIDController.setFeedbackDevice(encoder);
+        double kP = 0.1;
+        double kI = 0;
+        double kD = 0;
+        double kIz = 0;
+        double kFF = 0;
+        double kMaxOutput = 1;
+        double kMinOutput = -1;
+        int smartMotionSlot = 0;
+        double kMaxVel = 2000;
+        double kMinVel = 0;
+        double kMaxAcc = 1500;
+        PIDController.setP(kP);
+        PIDController.setI(kI);
+        PIDController.setD(kD);
+        PIDController.setIZone(kIz);
+        PIDController.setFF(kFF);
+        PIDController.setOutputRange(kMinOutput, kMaxOutput);
+        PIDController.setReference(0, CANSparkMax.ControlType.kSmartMotion);
+        PIDController.setSmartMotionMaxVelocity(kMaxVel, smartMotionSlot);
+        PIDController.setSmartMotionMinOutputVelocity(kMinVel, smartMotionSlot);
+        PIDController.setSmartMotionMaxAccel(kMaxAcc, smartMotionSlot);
+        PIDController.setSmartMotionAllowedClosedLoopError(kMinOutput, smartMotionSlot);
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
+
+        // display Smart Motion coefficients
+        SmartDashboard.putNumber("Max Velocity", kMaxVel);
+        SmartDashboard.putNumber("Min Velocity", kMinVel);
+        SmartDashboard.putNumber("Max Acceleration", kMaxAcc);
+        SmartDashboard.putNumber("Allowed Closed Loop Error", kMinOutput);
+        SmartDashboard.putNumber("Set Position", 0);
+        SmartDashboard.putNumber("Set Velocity", 0);
+
+        // button to toggle between velocity and smart motion modes
+        SmartDashboard.putBoolean("Mode", true);
+        return PIDController;
+    }
+
+    // creates and sets a PID controller
+    private SparkMaxPIDController controllerMotorFirst = createAndSetupPIDController(motorFirst);
+
+    // gets the number of rotations from the encoder
+    private double getPosition() {
+        return encoder.getPosition();
+    }
+
+    // Sets the setPoint of the PID controller to whatever was passed as the input
+    private void setPosition(double length) {
+        controllerMotorFirst.setReference(length, CANSparkMax.ControlType.kPosition);
+    }
+
+    // resets the encoder
+    private void zeroEncoder() {
+        if (limitSwitchMotorFirst.isPressed()) {
+            encoder.setPosition(0);
+        } else {
+            System.out.println("limit switch is no pressy");
+        }
+    }
+
+    // fully retracts elevator
+    private void fullyRetract() {
+        setPosition(0);
+        zeroEncoder();
+    }
+
+    @Override
+    public void periodic() {
+        fullyRetract();
+    }
+}
