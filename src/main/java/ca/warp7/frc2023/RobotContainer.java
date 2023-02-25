6a96f1174b5d779a94ad85be1890981792a80536
Colepng <gamingwithcole7@gmail.com>
2023-02-24 20:41:16 -0500

    Add auto chooser

--- src/main/java/ca/warp7/frc2023/RobotContainer.java
+++ src/main/java/ca/warp7/frc2023/RobotContainer.java
@@ -4,17 +4,31 @@
 
 package ca.warp7.frc2023;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
+
+    // create a chooser for autonomous commands
+    SendableChooser<Command> m_chooser = new SendableChooser<>();
+
     public RobotContainer() {
         configureBindings();
+
+        // add options to the chooser
+        m_chooser.setDefaultOption("Default Auto", Commands.print("Default Auto"));
+        m_chooser.addOption("Other auto", null);
+
+        // put the chooser on the dashboard
+        SmartDashboard.putData(m_chooser);
     }
 
     private void configureBindings() {}
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        // return the selected command
+        return m_chooser.getSelected();
     }
 }
