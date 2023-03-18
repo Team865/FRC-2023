0b6b8ffd2ca66f39e117f346679ac0aeb4a60210
Dangosaurus <41658209+Dangosaurus@users.noreply.github.com>
2023-03-18 02:03:27 -0400

    Changed common files
    Squash commit for common files pre-georgian and during georgian
    
    Co-authored-by: png <gamingwithcole7@gmail.com>

--- src/main/java/ca/warp7/frc2023/Constants.java
+++ src/main/java/ca/warp7/frc2023/Constants.java
@@ -1,15 +1,195 @@
 package ca.warp7.frc2023;
 
+import ca.warp7.frc2023.lib.config.SVAConfig;
+import ca.warp7.frc2023.lib.config.SwerveModuleConfig;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.ctre.phoenix.sensors.SensorTimeBase;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 public class Constants {
 
-    // fourbar constants
-    public static final int kFourbarMotorPortID0 = 0;
-    public static final int kFourbarMotorPortID1 = 1;
+    /* Drivetrain Information */
+    public static final class kDrivetrain {
+        public static final double kTrackWidth = Units.inchesToMeters(30.75);
+        public static final double kWheelBase = Units.inchesToMeters(18.75);
+        public static final double kWheelDiameter = Units.inchesToMeters(4);
+        public static final double kWheelCircumference = kWheelDiameter * Math.PI;
+        public static final double kMaxSpeed = Units.feetToMeters(16.3);
+        // This should calculate the theoretical max angular velocity using the max speed and the longer radius of the
+        // robot
+        public static final double kMaxAngularVelocity = kMaxSpeed / (kTrackWidth / 2);
+        public static final double kDriveGearRatio = 6.75 / 1;
+        public static final double kSteerGearRatio = (150.0 / 7.0) / 1.0;
 
-    // Replace with real can id's
-    public static final int kMotorFirst = 11;
-    public static final int kMotorSecond = 12;
+        public static final SVAConfig kDriveFeedforward = new SVAConfig(0, 0, 0);
 
-    public static final int kMotorThird = 21;
-    public static final int kMotorFourth = 22;
+        public static final SwerveDriveKinematics kSwerveDriveKinematics = new SwerveDriveKinematics(
+                new Translation2d(kDrivetrain.kTrackWidth / 2.0, kDrivetrain.kWheelBase / 2.0),
+                new Translation2d(kDrivetrain.kTrackWidth / 2.0, -kDrivetrain.kWheelBase / 2.0),
+                new Translation2d(-kDrivetrain.kTrackWidth / 2.0, kDrivetrain.kWheelBase / 2.0),
+                new Translation2d(-kDrivetrain.kTrackWidth / 2.0, -kDrivetrain.kWheelBase / 2.0));
+
+        private static final CANCoderConfiguration kSteerEncoderConfig = new CANCoderConfiguration() {
+            {
+                absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+                sensorDirection = false;
+                initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+                sensorTimeBase = SensorTimeBase.PerSecond;
+            }
+        };
+
+        private static final SupplyCurrentLimitConfiguration kDriveSupplyLimit =
+                new SupplyCurrentLimitConfiguration(true, 35, 60, 0.1);
+
+        private static final TalonFXConfiguration kDriveConfig = new TalonFXConfiguration() {
+            {
+                slot0.kP = 0;
+                slot0.kI = 0;
+                slot0.kD = 0;
+                slot0.kF = 0;
+                supplyCurrLimit = kDriveSupplyLimit;
+                voltageCompSaturation = 12.0;
+                // openloopRamp = 0;
+                // closedloopRamp = 0;
+            }
+        };
+
+        private static final SupplyCurrentLimitConfiguration kAngleSupplyLimit =
+                new SupplyCurrentLimitConfiguration(true, 25, 40, 0.1);
+
+        private static final TalonFXConfiguration kSteerConfig = new TalonFXConfiguration() {
+            {
+                slot0.kP = 0.25;
+                slot0.kI = 0.0;
+                slot0.kD = 0.05;
+                supplyCurrLimit = kAngleSupplyLimit;
+                voltageCompSaturation = 12.0;
+            }
+        };
+
+        public static final class kModule0 {
+            private static final int kModuleID = 0;
+            private static final String kFriendlyName = "Front left";
+            private static final int kDriveMotorID = 22;
+            private static final int kSteerMotorID = 21;
+            private static final int kSteerEncoderID = 20;
+            private static final double kSteerEncoderOffset = 4.86;
+
+            public static final SwerveModuleConfig kConfig = new SwerveModuleConfig(
+                    kModuleID,
+                    kFriendlyName,
+                    kDriveMotorID,
+                    kSteerMotorID,
+                    kSteerEncoderID,
+                    kSteerEncoderOffset,
+                    kSteerEncoderConfig,
+                    kDriveConfig,
+                    kSteerConfig,
+                    kDriveFeedforward);
+        }
+
+        public static final class kModule1 {
+            private static final int kModuleID = 1;
+            private static final String kFriendlyName = "Front right";
+            private static final int kDriveMotorID = 32;
+            private static final int kSteerMotorID = 31;
+            private static final int kSteerEncoderID = 30;
+            private static final double kSteerEncoderOffset = 141;
+
+            public static final SwerveModuleConfig kConfig = new SwerveModuleConfig(
+                    kModuleID,
+                    kFriendlyName,
+                    kDriveMotorID,
+                    kSteerMotorID,
+                    kSteerEncoderID,
+                    kSteerEncoderOffset,
+                    kSteerEncoderConfig,
+                    kDriveConfig,
+                    kSteerConfig,
+                    kDriveFeedforward);
+        }
+
+        public static final class kModule2 {
+            private static final int kModuleID = 2;
+            private static final String kFriendlyName = "Back left";
+            private static final int kDriveMotorID = 42;
+            private static final int kSteerMotorID = 41;
+            private static final int kSteerEncoderID = 40;
+            private static final double kSteerEncoderOffset = 52.91;
+
+            public static final SwerveModuleConfig kConfig = new SwerveModuleConfig(
+                    kModuleID,
+                    kFriendlyName,
+                    kDriveMotorID,
+                    kSteerMotorID,
+                    kSteerEncoderID,
+                    kSteerEncoderOffset,
+                    kSteerEncoderConfig,
+                    kDriveConfig,
+                    kSteerConfig,
+                    kDriveFeedforward);
+        }
+
+        public static final class kModule3 {
+            private static final int kModuleID = 3;
+            private static final String kFriendlyName = "Back right";
+            private static final int kDriveMotorID = 52;
+            private static final int kSteerMotorID = 51;
+            private static final int kSteerEncoderID = 50;
+            private static final double kSteerEncoderOffset = 283.5;
+
+            public static final SwerveModuleConfig kConfig = new SwerveModuleConfig(
+                    kModuleID,
+                    kFriendlyName,
+                    kDriveMotorID,
+                    kSteerMotorID,
+                    kSteerEncoderID,
+                    kSteerEncoderOffset,
+                    kSteerEncoderConfig,
+                    kDriveConfig,
+                    kSteerConfig,
+                    kDriveFeedforward);
+        }
+    }
+
+    public static final class kTeleop {
+        public static final double kStickDeadband = 0.15;
+        public static final double kTriggerDeadband = 0.05;
+    }
+
+    public static final class kAuton {
+        public static final double kOuttakeSpeed = -0.8;
+    }
+
+    /* Controller USB Order */
+    public static final class kControllers {
+        public static final int kDriverControllerID = 0;
+        public static final int kOperatorControllerID = 1;
+        public static final int kTechnicianControllerID = 2;
+    }
+
+    public static final class kFourbar {
+        public static final int kFourbarMotorLeftID = 13;
+        public static final int kFourbarMotorRightID = 10;
+    }
+
+    public static final class kElevator {
+        public static final int kLeftPrimaryMotorID = 16;
+        public static final int kLeftSecondaryMotorID = 17;
+        public static final int kRightPrimaryMotorID = 14;
+        public static final int kRightSecondaryMotorID = 15;
+    }
+
+    public static final class kIntake {
+        public static final double kMaxIntakePercentOut = 0.8;
+        public static final int kFrontWheelMotorID = 11;
+        public static final int kRearWheelMotorID = 18;
+        public static final int kTalonPivotMotorID = 12;
+    }
 }
--- src/main/java/ca/warp7/frc2023/RobotContainer.java
+++ src/main/java/ca/warp7/frc2023/RobotContainer.java
@@ -4,38 +4,90 @@
 
 package ca.warp7.frc2023;
 
+import ca.warp7.frc2023.Constants.kControllers;
+import ca.warp7.frc2023.commands.TeleopDriveCommand;
+import ca.warp7.frc2023.commands.TeleopElevatorCommand;
+import ca.warp7.frc2023.commands.TeleopFourbarCommand;
+import ca.warp7.frc2023.commands.TeleopIntakeCommand;
+import ca.warp7.frc2023.commands.auton.MobiltyCone;
+import ca.warp7.frc2023.commands.auton.SimpleConeAuto;
+import ca.warp7.frc2023.subsystems.ElevatorSubsystem;
+import ca.warp7.frc2023.subsystems.FourbarSubsystem;
+import ca.warp7.frc2023.subsystems.IntakeSubsystem;
+import ca.warp7.frc2023.subsystems.SwerveDrivetrainSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
+    private final CommandXboxController primaryOperatorController =
+            new CommandXboxController(kControllers.kDriverControllerID);
+    private final CommandXboxController secondaryOperatorController =
+            new CommandXboxController(kControllers.kOperatorControllerID);
+    private final CommandXboxController technicianController =
+            new CommandXboxController(kControllers.kTechnicianControllerID);
 
-    // create a chooser for autonomous commands
-    SendableChooser<Command> m_chooser = new SendableChooser<>();
+    private final SwerveDrivetrainSubsystem swerveDrivetrainSubsystem = new SwerveDrivetrainSubsystem();
+    private final FourbarSubsystem fourbarSubsystem = new FourbarSubsystem();
+    private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+    private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+
+    private final SendableChooser<Command> autoChooser = new SendableChooser<>();
 
     public RobotContainer() {
+        swerveDrivetrainSubsystem.setDefaultCommand(new TeleopDriveCommand(
+                swerveDrivetrainSubsystem,
+                () -> -primaryOperatorController.getLeftY(),
+                () -> -primaryOperatorController.getLeftX(),
+                () -> -primaryOperatorController.getRightX(),
+                () -> primaryOperatorController.leftBumper().getAsBoolean(),
+                () -> primaryOperatorController.rightBumper().getAsBoolean()));
+
+        intakeSubsystem.setDefaultCommand(new TeleopIntakeCommand(
+                intakeSubsystem,
+                () -> secondaryOperatorController.rightBumper().getAsBoolean(),
+                () -> secondaryOperatorController.leftBumper().getAsBoolean(),
+                () -> secondaryOperatorController.getRightTriggerAxis()));
+
+
+        fourbarSubsystem.setDefaultCommand(
+                new TeleopFourbarCommand(fourbarSubsystem, () -> secondaryOperatorController.getLeftY()));
+
+        elevatorSubsystem.setDefaultCommand(
+                new TeleopElevatorCommand(elevatorSubsystem, () -> secondaryOperatorController.getRightY()));
 
         configureBindings();
 
-        // add options to the chooser
-        m_chooser.setDefaultOption("Default Auto", Commands.print("Default Auto"));
-        m_chooser.addOption("Other auto", null);
+        configureAuto();
+    }
 
-        // put the chooser on the dashboard
-        SmartDashboard.putData(m_chooser);
+    private void configureAuto() {
+        autoChooser.setDefaultOption("NO AUTO!", Commands.print("No auto selected"));
+        autoChooser.addOption("Simple cone auto", new SimpleConeAuto(intakeSubsystem));
+        autoChooser.addOption("Cone and mobilty", new MobiltyCone(intakeSubsystem, swerveDrivetrainSubsystem));
+        SmartDashboard.putData("autoChooser", autoChooser);
     }
 
     private void configureBindings() {
-        controller.b().toggleOnTrue(intakeing.toggle());
-        controller.x().toggleOnTrue(intakeing.movemotorCommand());
-        controller.a().toggleOnTrue(intakeing.stopmotorCommand());
-        controller.y().toggleOnTrue(intakeing.closeInkate());
+        primaryOperatorController.back().onTrue(new InstantCommand(swerveDrivetrainSubsystem::zeroGyro));
+        primaryOperatorController
+                .leftStick()
+                .onTrue(new InstantCommand(swerveDrivetrainSubsystem::resetSwerveModulesToAbsolute));
+        primaryOperatorController.a().onTrue(new InstantCommand(swerveDrivetrainSubsystem::brake));
+
+        secondaryOperatorController.a().onTrue(fourbarSubsystem.setPosition(0));
+        secondaryOperatorController.x().onTrue(fourbarSubsystem.setPosition(25));
+        secondaryOperatorController.y().onTrue(fourbarSubsystem.setPosition(57.5));
+
+        secondaryOperatorController.povDown().onTrue(intakeSubsystem.setTalonPivotSetPoint(-5));
+        secondaryOperatorController.povLeft().onTrue(intakeSubsystem.setTalonPivotSetPoint(-15));
+        secondaryOperatorController.povUp().onTrue(intakeSubsystem.setTalonPivotSetPoint(-25));
     }
 
     public Command getAutonomousCommand() {
-        // return the selected command
-        return m_chooser.getSelected();
+        return autoChooser.getSelected();
     }
 }
