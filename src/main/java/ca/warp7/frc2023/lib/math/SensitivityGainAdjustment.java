65880f92841daffa40cfb90cb5017c933b81c376
Dangosaurus <41658209+Dangosaurus@users.noreply.github.com>
2023-03-29 15:40:23 -0400

    Change drive gain adjustment
    Made it more sensitive near 0

--- src/main/java/ca/warp7/frc2023/lib/math/SensitivityGainAdjustment.java
+++ src/main/java/ca/warp7/frc2023/lib/math/SensitivityGainAdjustment.java
@@ -3,6 +3,6 @@ package ca.warp7.frc2023.lib.math;
 public class SensitivityGainAdjustment {
 
     public static double driveGainAdjustment(double input) {
-        return ((0.35) * (input)) + ((.65) * (Math.pow(input, 3)));
+        return ((0.45) * (input)) + ((.55) * (Math.pow(input, 3)));
     }
 }
