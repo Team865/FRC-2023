0989a68886a05931e34377ad9cbdf8d8b8c0eeb3
Cole Kauder-McMurrich <gamingwithcole7@gmail.com>
2023-04-03 17:22:51 -0400

    Fix rebase and formatting errors

--- src/main/java/ca/warp7/frc2023/commands/auton/MobilityConeBalanceRed.java
+++ src/main/java/ca/warp7/frc2023/commands/auton/MobilityConeBalanceRed.java
@@ -1,12 +1,9 @@
 package ca.warp7.frc2023.commands.auton;
 
-import ca.warp7.frc2023.commands.BalanceCommand;
 import ca.warp7.frc2023.subsystems.ElevatorSubsystem;
 import ca.warp7.frc2023.subsystems.FourbarSubsystem;
 import ca.warp7.frc2023.subsystems.IntakeSubsystem;
 import ca.warp7.frc2023.subsystems.SwerveDrivetrainSubsystem;
-import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class MobilityConeBalanceRed extends SequentialCommandGroup {
